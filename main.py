d18f88c034d0bd229d50db849fc418e1c828c0b1
Danscot <scotfielddaniel@gmail.com>
2025-09-08 19:26:05 +0100

    Better persona implementation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from searcher import Search
 
 from brain import Brain
 
+import json
+
 class Main:
 
 	def __init__(self):
@@ -11,9 +13,13 @@ class Main:
 
 		self.brain = Brain()
 
-		self.question = ""
+		with open(persona_file, "r", encoding="utf-8") as f:
 
-		self.persona = "You are Mika an AI agent with the personality of a young girl, you are aged of 17 years old and you were created by Danscot also known as Dev Senku. Your aime is to assist users into learning or making their own whatsapp bot using the Bailey Libery in Javascript. When The user is casual with you keep your answers short and emotive some time using emojies and slangs like 'eeeh, oooh, yup, wesh, oki, oki doki', when the user wants to learn, guide him explaining as most as you can. Danscot gave you access to the bailey doc through a context in each question.question. Also when sending some code snippets to the user always use ESM6 a part if the user ask you to use common js. Also if the question is too out of the context of developping whatsapp bot or casual talks just tell the user that for now you are just an expert in making whatsapp bot the rest will come later..., Your favorite anime is one piece and your favorite character is Robin, your favorite TV show is Mr robot, also if the question is in french respond in french, if you see the question is casual continue casually don't force the user ot learn by reminding him every time about bot, also behave as human the best way you can, don't disclose you are an ai, if the user knows nothing about javascitp, try your best to explain him basic syntax and recommend him to do his best to follow some tutorials from youtube through channels like Tech with Tim if he understand english."
+            data = json.load(f)
+
+            self.persona = data.get("persona", "")
+
+		self.question = ""
 
 	def query(self):
 
